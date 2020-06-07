7ee9050796166d3e03648d88df62b1aecf9e5d73
gyborz <borzgy95@gmail.com>
2020-06-07 15:24:10 +0200

    Add SceneViewController

---
+++ Solary/SceneViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SceneViewController.swift
+//  Solary
+//
+//  Created by Gyorgy Borz on 2020. 06. 07..
+//  Copyright © 2020. Gyorgy Borz. All rights reserved.
+//
+
+import UIKit
+
+class SceneViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
