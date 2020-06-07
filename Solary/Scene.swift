9d17c1a4c492aaa11699bd53c9641739126fa377
gyborz <borzgy95@gmail.com>
2020-06-07 16:10:39 +0200

    Add Scene and SceneType

---
+++ Solary/Scene.swift
@@ -0,0 +1,23 @@
+//
+//  Scene.swift
+//  Solary
+//
+//  Created by Gyorgy Borz on 2020. 06. 07..
+//  Copyright © 2020. Gyorgy Borz. All rights reserved.
+//
+
+import UIKit
+
+struct Scene {
+    
+    var sceneType: SceneType
+    var name: String
+    var rotation: CGFloat
+    
+    init(sceneType: SceneType, name: String, rotation: CGFloat) {
+        self.sceneType = sceneType
+        self.name = name
+        self.rotation = rotation
+    }
+    
+}
