585bacc96f3b57bd57c427926533419801b521d5
gyborz <borzgy95@gmail.com>
2020-06-09 19:01:08 +0200

    Add pointer node, function for state tracking

--- Solary/ARViewController.swift
+++ Solary/ARViewController.swift
@@ -13,6 +13,18 @@ import ARKit
 class ARViewController: UIViewController, ARSCNViewDelegate {
     
     var node: Node!
+    private var center: CGPoint!
+    private var positions = [SCNVector3]()
+    private let pointer = SCNScene(named: "art.scnassets/pointer.scn")!.rootNode
+    private var rootNode: SCNNode {
+        return sceneView.scene.rootNode
+    }
+    
+    private enum SceneState {
+        case pointer
+        case planet
+    }
+    private var currentSceneState: SceneState = .pointer
     
     override var prefersStatusBarHidden: Bool {
         return true
@@ -25,8 +37,7 @@ class ARViewController: UIViewController, ARSCNViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         sceneView.delegate = self
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        sceneView.scene = scene
+        center = view.center
         setupUI()
     }
     
@@ -61,6 +72,7 @@ class ARViewController: UIViewController, ARSCNViewDelegate {
     private func setupARSession() {
         let configuration = ARWorldTrackingConfiguration()
         sceneView.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
+        rootNode.addChildNode(pointer)
     }
     
     private func setOverlay(automatically: Bool, forDetectionType goal: ARCoachingOverlayView.Goal) {
@@ -80,6 +92,23 @@ class ARViewController: UIViewController, ARSCNViewDelegate {
         self.coachingOverlay.activatesAutomatically = automatically
         
         self.coachingOverlay.goal = goal
+    }
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        center = view.center
+    }
+    
+    private func getAveragePosition(from positions: Array<SCNVector3>.SubSequence) -> SCNVector3 {
+        var averageX: Float = 0
+        var averageY: Float = 0
+        var averageZ: Float = 0
+        for position in positions {
+            averageX += position.x
+            averageY += position.y
+            averageZ += position.z
+        }
+        let count = Float(positions.count)
+        return SCNVector3Make(averageX / count, averageY / count, averageZ / count)
     }
 
     // MARK: - ARSCNViewDelegate
@@ -101,6 +130,34 @@ class ARViewController: UIViewController, ARSCNViewDelegate {
     
     func sessionShouldAttemptRelocalization(_ session: ARSession) -> Bool {
         true
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
+        let hitTest = sceneView.hitTest(center, types: .featurePoint)
+        let result = hitTest.last
+        guard let transform = result?.worldTransform else { return }
+        let thirdColumn = transform.columns.3
+        let position = SCNVector3Make(thirdColumn.x, thirdColumn.y, thirdColumn.z)
+        positions.append(position)
+        let lastTenPositions = positions.suffix(10)
+        pointer.position = getAveragePosition(from: lastTenPositions)
+    }
+    
+    func session(_ session: ARSession, cameraDidChangeTrackingState camera: ARCamera) {
+        switch camera.trackingState {
+        case .notAvailable, .limited:
+            // MARK: - TODO
+            print("not normal")
+            if currentSceneState == .pointer {
+                pointer.removeFromParentNode()
+            }
+        case .normal:
+            // MARK: - TODO
+            print("normal")
+            if currentSceneState == .pointer {
+                rootNode.addChildNode(pointer)
+            }
+        }
     }
     
     @IBAction func exitButtonTapped(_ sender: UIButton) {
